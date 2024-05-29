b48ce3f5a25aacf28274b128001b87c8f766b612
Shithi Maitra <43873081+shithi30@users.noreply.github.com>
2024-05-28 22:29:13 -0400

    Add files via upload

---
+++ competitor_vs_unilever_jargon_wordclouds.py
@@ -0,0 +1,282 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# import
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from bs4 import BeautifulSoup
+import pandas as pd
+import duckdb
+import multiprocessing
+from tabulate import tabulate
+from wordcloud import WordCloud, STOPWORDS
+import time
+import win32com.client
+from pretty_html_table import build_table
+# just in case
+import base64
+import random
+
+
+# In[ ]:
+
+
+## Chaldal ##
+def scrape_chaldal_process(keywords): 
+    
+    # accumulators
+    brands = ['Boost', 'Clear Shampoo', 'Clear Men Shampoo', 'Clear Hijab', 'Simple', 'Pepsodent', 'Brylcreem', 'Bru', 'St. Ives', 'Horlicks', 'Sunsilk', 'Lux', 'Ponds', "Pond's", 'Closeup', 'Cif', 'Dove', 'Maltova', 'Domex', 'Clinic', 'Tresemme', 'Tresemmé', 'GlucoMax', 'Knorr', 'Glow & Lovely', 'Glow & Handsome', 'Wheel', 'Axe', 'Pureit', 'Lifebuoy', 'Surf Excel', 'Vaseline', 'Vim', 'Rin']
+    df_acc_local = pd.DataFrame()
+    lock = multiprocessing.Lock()
+
+    # open window
+    driver = webdriver.Chrome('chromedriver', options=[])
+    driver.maximize_window()
+
+    for k in keywords:
+        # url
+        url = "https://chaldal.com/search/" + k
+        driver.get(url)
+        
+        # scroll
+        SCROLL_PAUSE_TIME = 5
+        last_height = driver.execute_script("return document.body.scrollHeight")
+        while True:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(SCROLL_PAUSE_TIME)
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height: break
+            last_height = new_height
+
+        # soup
+        soup_init = BeautifulSoup(driver.page_source, 'html.parser')
+        soup = soup_init.find_all("div", attrs={"class": "imageWrapper"})
+        
+        # scrape
+        skus = []
+        quants = []
+        prices = []
+        prices_if_discounted = []
+        for s in soup:
+            # sku
+            try: val = s.find("div", attrs={"class": "name"}).get_text()
+            except: val = None
+            skus.append(val)
+            # quantity
+            try: val = s.find("div", attrs={"class": "subText"}).get_text()
+            except: val = None
+            quants.append(val)
+            # price
+            try: val = float(s.find("div", attrs={"class": "price"}).get_text().split()[1].replace(',', ''))
+            except: val = None
+            prices.append(val)
+            # discount
+            try: val = float(s.find("div", attrs={"class": "discountedPrice"}).get_text().split()[1].replace(',', ''))
+            except: val = None
+            prices_if_discounted.append(val)
+        
+        # accumulate
+        df = pd.DataFrame()
+        df['sku'] = skus
+        df['keyword'] = k
+        df['quantity'] = quants
+        df['price'] = prices
+        df['price_if_discounted'] = prices_if_discounted
+        
+        # relevant data
+        qry = '''
+        select *
+        from
+            (select *, row_number() over() pos_in_pg
+            from df
+            ) tbl1 
+        where replace(sku, ' ', '') ilike ''' + "'%" + k.replace(" ", "") + '''%';
+        '''
+        df = duckdb.query(qry).df()
+        rel_idx = df['pos_in_pg'].tolist()
+        len_rel_idx = len(rel_idx)
+        
+        # description
+        bnr = 3
+        try: driver.find_element(By.CLASS_NAME, "important-banner")
+        except: bnr = 2
+        descs = []
+        report_times = []
+        for i in range(0, len_rel_idx): 
+            report_times.append(time.strftime('%Y-%m-%d %H:%M:%S'))
+            descs.append("ERROR")
+            try:
+                # move
+                path = '//*[@id="page"]/div/div[6]/section/div/div/div/div/section/div['+str(bnr)+']/div[2]/div['+str(rel_idx[i])+']/div/div'
+                elem = driver.find_element(By.XPATH, path)
+                mov = ActionChains(driver).move_to_element(elem)
+                mov.perform()
+                # details
+                path1 = '//*[@id="page"]/div/div[6]/section/div/div/div/div/section/div['+str(bnr)+']/div[2]/div['+str(rel_idx[i])+']/div/div/div[5]/span/a'
+                path2 = '//*[@id="page"]/div/div[6]/section/div/div/div/div/section/div['+str(bnr)+']/div[2]/div['+str(rel_idx[i])+']/div/div/div[6]/span/a'
+                try: elem = driver.find_element(By.XPATH, path1)
+                except: elem = driver.find_element(By.XPATH, path2)
+                elem.click()
+                # content
+                path = '//*[@id="page"]/div/div[6]/section/div/div/div/div/section/div['+str(bnr)+']/div[2]/div['+str(rel_idx[i])+']/div/div[2]/div/div/article/section[2]/div[5]'
+                elem = driver.find_element(By.XPATH, path)
+                descs[i] = elem.text.replace("\n", " ")
+                # close
+                path = '//*[@id="page"]/div/div[6]/section/div/div/div/div/section/div['+str(bnr)+']/div[2]/div['+str(rel_idx[i])+']/div/div[2]/div/button'
+                elem = driver.find_element(By.XPATH, path)
+                elem.click()
+            except: pass
+            
+        # conglomerate
+        if_ubls = []
+        skus = [skus[i-1] for i in rel_idx]
+        for i in range(0, len_rel_idx):
+            if_ubls.append(None)
+            for b in brands:
+                if b.lower() + ' ' in skus[i].lower():
+                    if_ubls[i] = True
+                    break     
+        
+        # accumulate
+        df['if_unilever'] = if_ubls
+        df['description'] = descs
+        df['report_time'] = report_times
+        df_acc_local = df_acc_local.append(df)
+        
+        # progress
+        lock.acquire()
+        print("Data fetched for keyword: " + k)
+        lock.release()
+        
+    # close window
+    driver.close()
+    
+    # return
+    return df_acc_local
+    
+def scrape_chaldal(folder):
+    
+    # accumulators
+    start_time = time.time()
+    df_acc = pd.DataFrame()
+    keywords = ['conditioner', 'handwash', 'bodywash', 'facewash', 'lotion', 'cream', 'toothpaste', 'dishwash', 'toilet clean', 'soup', 'shampoo', 'health drink', 'detergent', 'moisturizer', 'soap', 'petroleum jelly', 'hair oil', 'germ kill']
+    process_count = 3
+    keywords_chunks = [keywords[i::process_count] for i in range(process_count)]
+    
+    # processes
+    pool = multiprocessing.Pool(process_count)
+    dfs_acc = pool.map(scrape_chaldal_process, keywords_chunks)
+    pool.close()
+    pool.join()
+    
+    # csv
+    for i in range(0, process_count): df_acc = df_acc.append(dfs_acc[i])
+    filename = folder + r"\chaldal_unilever_keywords_data.csv"    
+    df_acc.to_csv(filename, index=False)           
+    
+    # stats
+    print("\nTotal SKUs found: " + str(df_acc.shape[0]))
+    elapsed_time = str(round((time.time() - start_time) / 60.00, 2))
+    print("Elapsed time to run processes (mins): " + elapsed_time)
+    
+    # return
+    return df_acc
+
+
+# In[ ]:
+
+
+def send_viz(scraped_df, folder):
+    
+    # summary
+    qry = '''
+    select
+        'Chaldal' platform,
+        count(sku) "SKUs", 
+        count(case when if_unilever=true then sku else null end) "UBL SKUs", 
+        count(case when if_unilever=true then sku else null end)*1.00/count(sku) "SoS",
+        count(case when if_unilever=true and pos_in_pg<11 then sku else null end)*1.00/(count(distinct keyword)*10) "top-10 SoS", 
+        count(case when if_unilever=true and pos_in_pg<11 and length(description)=0 then sku else null end) "top-10 SoS missing description", 
+        count(case when if_unilever=true and length(description)=0 then sku else null end) "UBL SKUs missing description", 
+        count(case when description='ERROR' then sku else null end) "description errors"
+    from scraped_df; 
+    '''
+    anls_df = duckdb.query(qry).df()
+    print(tabulate(anls_df, headers='keys', tablefmt='psql', showindex=False))
+    
+    # email
+    ol = win32com.client.Dispatch("outlook.application")
+    olmailitem = 0x0
+    newmail = ol.CreateItem(olmailitem)
+
+    # subject, recipients
+    newmail.Subject = 'Chaldal Keyword SoS & Wordcloud'
+    newmail.To = 'shithi.maitra@unilever.com'
+    # newmail.To = 'mehedi.asif@unilever.com'
+    # newmail.CC = 'mehedi.asif@unilever.com; zakeea.husain@unilever.com; rakaanjum.unilever@gmail.com; nazmussajid.ubl@gmail.com'
+    
+    # UBL cloud
+    qry = ''' select description from scraped_df where if_unilever=true; '''
+    df = duckdb.query(qry).df()
+    text = " ".join(d for d in df['description'].fillna("").tolist())
+    word_cloud = WordCloud(width=300, height=200, random_state=1, background_color="white", colormap="ocean", collocations=False, stopwords=STOPWORDS).generate(text)
+    word_cloud.to_file(folder + r"\ubl_cloud.png")
+    cloud1_html = f'<img src="cid: MyId1" style="border: 1px solid; padding: 5px; background-color: white; display: block" width="96%" height="100%"><figcaption><b>Fig-01:</b> UBL wordcloud</figcaption>'
+
+    # non UBL cloud
+    qry = ''' select description from scraped_df where if_unilever is null; '''
+    df = duckdb.query(qry).df()
+    text = " ".join(d for d in df['description'].fillna("").tolist())
+    word_cloud = WordCloud(width=300, height=200, random_state=1, background_color="white", colormap="Set2", collocations=False, stopwords=STOPWORDS).generate(text)
+    word_cloud.to_file(folder + r"\nonubl_cloud.png")
+    cloud2_html = f'<img src="cid: MyId2" style="border: 1px solid; padding: 5px; background-color: white; display: block" width="96%" height="100%"><figcaption><b>Fig-02:</b> non-UBL wordcloud</figcaption>'
+    
+    # body
+    newmail.HTMLbody = f'''
+    Dear concern,<br><br>
+    Today's <i>Chaldal</i> UBL share of search (SoS) and descriptions for selected keywords have been scraped. A brief statistics of the process is given below:
+    ''' + build_table(anls_df, 'blue_light') + '''
+    <table style="margin-left: auto; margin-right: auto">
+        <tr>
+            <td>''' + cloud1_html + '''</td>
+            <td>''' + cloud2_html + '''</td>
+        </tr>
+    </table>
+    <br>
+    Two wordclouds based on UBL and non-UBL descriptions are presented above. Please find the full dataset attached. Note, this email was auto generated using <i>win32com</i>.<br><br>
+    Thanks,<br>
+    Shithi Maitra<br>
+    Asst. Manager, Cust. Service Excellence<br>
+    Unilever BD Ltd.<br>
+    '''
+    
+    # attachment
+    filename = folder + r"\chaldal_unilever_keywords_data.csv"
+    newmail.Attachments.Add(filename)
+    newmail.Attachments.Add(folder + r"\ubl_cloud.png").PropertyAccessor.SetProperty("http://schemas.microsoft.com/mapi/proptag/0x3712001F", "MyId1")
+    newmail.Attachments.Add(folder + r"\nonubl_cloud.png").PropertyAccessor.SetProperty("http://schemas.microsoft.com/mapi/proptag/0x3712001F", "MyId2")
+    
+    # send
+    newmail.Send()
+
+
+# In[ ]:
+
+
+# call
+if __name__ == "__main__":
+    folder = r"C:\Users\Shithi.Maitra\Unilever Codes\Scraping Scripts\Chaldal Stocks"
+    scraped_df = scrape_chaldal(folder)
+    send_viz(scraped_df, folder)
+
+
+# In[ ]:
+
+
+# run
+# "C:\Users\Shithi.Maitra\Downloads\chaldal_keywords_multi_process.py"
+
---
+++ national_daily_newspapers_scrape.py
@@ -0,0 +1,295 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+## import
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import duckdb
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+import time
+import win32com.client
+from pretty_html_table import build_table
+import random
+
+
+# In[2]:
+
+
+## scrape
+
+# newspapers
+prompts = ["The Financial Express", "The Daily Star", "The Business Standard", "Prothom Alo"]
+sources = ["thefinancialexpress.com", "thedailystar.net", "tbsnews.net", "prothomalo.com"]
+
+# accumulators
+start_time = time.time()
+df_acc = pd.DataFrame()
+timings = []
+
+# preference
+options = webdriver.ChromeOptions()
+options.add_argument("ignore-certificate-errors")
+
+# open window
+driver = webdriver.Chrome(service=Service(), options=options)
+driver.maximize_window()
+
+# iterate
+source_count = len(sources)
+for j in range(0, source_count):
+    
+    # link
+    link = "https://www.google.com/"
+    driver.get(link)
+
+    # search
+    print("Fetching articles from: " + sources[j])
+    elem = driver.find_element(By.CLASS_NAME, "gLFyf")
+    elem.send_keys(prompts[j] + " consumer goods FMCG news\n")
+
+    # scroll
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while(1):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(5)
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height: break
+        last_height = new_height
+
+    # soup
+    soup_init = BeautifulSoup(driver.page_source, "html.parser")
+    soup = soup_init.find_all("div", attrs={"class": "N54PNb BToiNc cvP2Ce"})
+
+    # scrape
+    headline = []
+    publish_date = []
+    excerpt = []
+    path = []
+    url = []
+    pos_in_search = []
+    report_date = []
+    news_count = len(soup)
+    for i in range(0, news_count):
+
+        # headline
+        try: val = soup[i].find("h3", attrs={"class": "LC20lb MBeuO DKV0Md"}).get_text()
+        except: val = None
+        headline.append(val)
+
+        # publication date
+        try: val = soup[i].find("span", attrs={"class": "lhLbod gEBHYd"}).get_text()
+        except: val = None
+        publish_date.append(val)
+
+        # excerpt
+        val = soup[i].find("div", attrs={"class": "kb0PBd cvP2Ce"}).get_text()
+        val = val.split(publish_date[i])[1] if publish_date[i] is not None else val
+        excerpt.append(val)
+
+        # path
+        try: val = soup[i].find("cite", attrs={"class": "qLRx3b tjvcx GvPZzd cHaqb"}).get_text()
+        except: val = None
+        path.append(val)
+        
+        # url
+        try: val = soup[i].find("a", attrs={"jsname": "UWckNb"})["href"]
+        except: val = None
+        url.append(val)
+
+        # position
+        pos_in_search.append(i + 1)
+        
+        # timing
+        timing = str(time.strftime('%Y-%m-%d %H:%M:%S'))
+        report_date.append(timing)
+
+    # accumulate 
+    df = pd.DataFrame()
+    df['headline'] = headline
+    df['publish_date'] = [p[0:-3] if p is not None else p for p in publish_date]
+    df['excerpt'] = excerpt
+    df['path'] = path
+    df['url'] = url
+    df['position'] = pos_in_search
+    df['newspaper'] = sources[j]
+    df['report_date'] = report_date
+    df = duckdb.query('''select * from df where path like '%''' + sources[j] +  '''%' ''').df()
+    df_acc = df_acc.append(df, ignore_index=True)
+    timings.append(timing)
+    
+# close window
+driver.close()
+
+
+# In[ ]:
+
+
+## GSheet
+
+# credentials
+SERVICE_ACCOUNT_FILE = "read-write-to-gsheet-apis-1-04f16c652b1e.json"
+SAMPLE_SPREADSHEET_ID = "1gkLRp59RyRw4UFds0-nNQhhWOaS4VFxtJ_Hgwg2x2A0"
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+
+# APIs
+creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+service = build("sheets", "v4", credentials=creds)
+sheet = service.spreadsheets()
+
+# extract
+values = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range='News!A1:I').execute().get('values', [])
+df_acc_prev = pd.DataFrame(values[1:], columns = values[0])
+
+# transform
+qry = '''
+-- old
+select headline, publish_date, excerpt, path, url, position, newspaper, 0 if_new, report_date
+from df_acc_prev
+union all
+-- new
+select 
+    headline, publish_date, excerpt, path, url, position, newspaper, 
+    case 
+        when publish_date like '%23' then 1
+        when publish_date like '%২৩' then 1
+        when publish_date like '%ago' then 1
+        when publish_date like '%আগে' then 1
+        else 0
+    end if_new, 
+    report_date
+from df_acc
+where url not in(select url from df_acc_prev)
+'''
+df_acc_pres = duckdb.query(qry).df()
+
+# load
+sheet.values().clear(spreadsheetId=SAMPLE_SPREADSHEET_ID, range='News').execute()
+sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID, range='News!A1', valueInputOption='USER_ENTERED', body={'values': [df_acc_pres.columns.values.tolist()] + df_acc_pres.fillna('').values.tolist()}).execute()
+
+
+# In[ ]:
+
+
+## novelty
+
+# new articles
+df_acc_new = duckdb.query('''select * from df_acc_pres where if_new=1''').df()
+new_heads = df_acc_new['headline'].tolist()
+new_links = df_acc_new['url'].tolist()
+new_dates = df_acc_new['publish_date'].tolist()
+new_len = df_acc_new.shape[0]
+
+# latest pull
+timing_df = pd.DataFrame()
+timing_df['source'] = sources
+timing_df['timing'] = timings
+
+# store
+if new_len > 0:
+    with pd.ExcelWriter("C:/Users/Shithi.Maitra/Downloads/newspaper_fmcg_scrapings.xlsx") as writer:
+        df_acc_pres.to_excel(writer, sheet_name="All Results", index=False)
+
+
+# In[ ]:
+
+
+## email
+
+# email
+ol = win32com.client.Dispatch("outlook.application")
+olmailitem = 0x0
+newmail = ol.CreateItem(olmailitem)
+
+# summary
+qry = '''
+select newspaper, total_articles, new_articles, last_report_time, latest_report_time
+from 
+    (select newspaper, count(url) total_articles, count(case when if_new=1 then url else null end) new_articles
+    from df_acc_pres
+    group by 1
+    ) tbl1 
+
+    inner join
+
+    (select newspaper, max(report_date) last_report_time
+    from df_acc_prev
+    group by 1
+    ) tbl2 using(newspaper)
+    
+    inner join
+
+    (select source newspaper, timing latest_report_time
+    from timing_df
+    ) tbl3 using(newspaper)
+'''
+summ_df = duckdb.query(qry).df()
+
+# subject, recipients
+newmail.Subject = 'Newspaper Scrapings - FMCG'
+# newmail.To = 'shithi.maitra@unilever.com'
+newmail.To = 'zoya.rashid@unilever.com'
+newmail.CC = 'mehedi.asif@unilever.com; samsuddoha.nayeem@unilever.com; sudipta.saha@unilever.com; asif.rezwan@unilever.com'
+
+# body
+newmail.HTMLbody = f'''
+Dear concern,<br><br>
+Please find recent FMCG articles from the popular English dailies attached. New articles are reported on <a href="https://teams.microsoft.com/l/channel/19%3ae8f5c9a9e7374b51840112d6280374af%40thread.tacv2/FMCG%2520News?groupId=1b8eee70-e11c-419e-966f-d830a968c87a&tenantId=f66fae02-5d36-495b-bfe0-78a6ff9f8e6e">Teams FMCG News</a>. Here are statistics from the latest pull: 
+''' + build_table(summ_df, random.choice(['green_light', 'red_light', 'blue_light', 'grey_light', 'orange_light']), font_size='12px', text_align='left') + '''
+More newspapers, online news portals or even Bangla dailies can be incorporated on demand. This is an auto-generated email via <i>win32com</i>.<br><br>
+Thanks,<br>
+Shithi Maitra<br>
+Asst. Manager, CSE<br>
+Unilever BD Ltd.<br>
+'''
+
+# attachment(s) 
+folder = "C:/Users/Shithi.Maitra/Downloads/"
+filename = folder + "newspaper_fmcg_scrapings.xlsx"
+newmail.Attachments.Add(filename)
+
+# send
+if new_len > 0: newmail.Send()
+
+
+# In[ ]:
+
+
+## MSTeams
+
+# email
+ol = win32com.client.Dispatch("outlook.application")
+olmailitem = 0x0
+newmail = ol.CreateItem(olmailitem)
+
+# report
+new = "⚠ The following " + str(new_len) + " article(s) are newly found, as of " + timing
+for i in range(0, new_len): new = new + '''<br>&nbsp;&nbsp;&nbsp;• <a href="''' + new_links[i] + '''">''' + new_heads[i] + '''</a> [''' + new_dates[i] + ''']''' 
+    
+# Teams
+newmail.Subject = "New FMCG Articles!"
+newmail.To = "FMCG News - Auto Monitoring <062c1c6b.Unilever.onmicrosoft.com@emea.teams.ms>"
+newmail.HTMLbody = new + "<br><br>"
+if new_len > 0: newmail.Send()
+
+
+# In[ ]:
+
+
+## stats
+display(df_acc_pres.head())
+print("Articles in result: " + str(df_acc_pres.shape[0]))
+print("Elapsed time to report (mins): " + str(round((time.time() - start_time) / 60.00, 2)))
+
+
+# In[ ]:
+
+
+
+
